0d0ca2967e547e8fb67d1c7a5104d73ac4f43c85
Khoa <ohwhynotme1999@gmail.com>
2020-11-12 11:27:53 +0700

    xong export email + nhúng cửa sủa đăng ký mail chimp

--- ASP.NET_CORE_Final_2019/ASP.NET_CORE_Final_2019/Areas/Admin/Controllers/KhachHangController.cs
+++ ASP.NET_CORE_Final_2019/ASP.NET_CORE_Final_2019/Areas/Admin/Controllers/KhachHangController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using ASP.NET_CORE_Final_2019.Areas.Services;
 using ASP.NET_CORE_Final_2019.Models;
+using ClosedXML.Excel;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASP.NET_CORE_Final_2019.Areas.Admin.Controllers
@@ -13,7 +16,7 @@ namespace ASP.NET_CORE_Final_2019.Areas.Admin.Controllers
     {
         public readonly IKhachHang IKhachHang;
         public readonly IDonHang IDonHang;
-        public KhachHangController(IKhachHang _IKhachHang,IDonHang _IDonHang)
+        public KhachHangController(IKhachHang _IKhachHang, IDonHang _IDonHang)
         {
             IKhachHang = _IKhachHang;
             IDonHang = _IDonHang;
@@ -35,6 +38,36 @@ namespace ASP.NET_CORE_Final_2019.Areas.Admin.Controllers
         {
             IKhachHang.UpdateKhachHang(_KhachHang);
             return RedirectToAction("Index");
+        }
+        public IActionResult ExportCSV()
+        {
+            var builder = new StringBuilder();
+            IEnumerable<Khachhang> listemail = IKhachHang.GetKhachhangs;
+            foreach (var item in listemail)
+            {
+                builder.AppendLine($"{item.Email}");
+            }
+            return File(Encoding.UTF8.GetBytes(builder.ToString()), "text/csv", "DanhSachEmailKhachHang.csv");
+        }
+        public IActionResult ExportXlsx()
+        {
+            IEnumerable<Khachhang> listemail = IKhachHang.GetKhachhangs;
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Emails");
+                var row = 1;
+                foreach (var item in listemail)
+                {
+                    worksheet.Cell(row, 1).Value = item.Email;
+                    row++;
+                }
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "DanhSachEmailKhachHang.xlsx");
+                }
+            }
         }
     }
 }
